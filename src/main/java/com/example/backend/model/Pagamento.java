2842c0a4292053b750f36dca878f4a51f8aba294
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-06-09 17:18:29 -0300

    model Pagamento

--- src/main/java/com/example/backend/model/Pagamento.java
+++ src/main/java/com/example/backend/model/Pagamento.java
@@ -2,29 +2,24 @@ package com.example.backend.model;
 
 import java.time.LocalDate;
 
-import org.hibernate.annotations.ManyToAny;
-
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
-@Entity
-public class Assinatura {
+public class Pagamento {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
 
     @ManyToOne
-    @JoinColumn(name = "cod_App")
-    private Aplicativo aplicativo;
+    @JoinColumn(name = "cod_Assinatura")
+    private Assinatura assinatura;
 
-    @ManyToOne
-    @JoinColumn(name = "cod_Cli")
-    private Cliente cliente;
+    private Double valorPago;
 
-    private LocalDate inicioVigencia;
-    private LocalDate fimVigencia;
+    private LocalDate dataPagamento;
+    
 }
