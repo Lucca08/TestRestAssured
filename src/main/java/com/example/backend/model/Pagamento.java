111a97133c45714139d8f758e4001f8ac89fc8fe
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-06-09 17:19:48 -0300

    model Pagamento ajuste

--- src/main/java/com/example/backend/model/Pagamento.java
+++ src/main/java/com/example/backend/model/Pagamento.java
@@ -2,12 +2,14 @@ package com.example.backend.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Pagamento {
 
     @Id
